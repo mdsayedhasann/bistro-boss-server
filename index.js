7690e62f21e7d259e655328c4beaf7aad7e63729
sayed_hossain <sayed.hasan0011@gmail.com>
2023-11-28 02:13:07 +0600

    Card backend code push

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000
 
 // MiddleWares
@@ -137,7 +138,21 @@ async function run() {
     // Cart Ended
 
 
+    // Payment Intent Start
+    app.post('/create-payment-intent', async(req, res) => {
+      const {price} = req.body;
+      const amount = parseInt(price * 100)
 
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+    // Payment Intent End
 
 
 
